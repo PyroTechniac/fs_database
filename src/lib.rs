94694682fa7d65304245d74b678ad91ae5708bf5
Gryffon Bellish <owenbellish@gmail.com>
2019-12-19 09:28:45 -0500

    initial things

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,34 @@
-pub struct Rectangle {
-    pub width: u32,
-    pub height: u32
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+pub struct Database {
+    file: File,
 }
 
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+impl Database {
+    pub fn new(raw: &str) -> Result<Database, &'static str> {
+        let path = PathBuf::from(raw);
+
+        if path.is_dir() {
+            return Err("Expected a path to a file, got a directory.");
+        }
+
+        let file = match path.canonicalize() {
+            Ok(f) => OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(false)
+                .open(f)
+                .expect("Failed to open file"),
+            Err(_) => OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(raw)
+                .expect("Failed to create file"),
+        };
+
+        Ok(Database { file })
     }
 }
